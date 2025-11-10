dff82c0e8319d299a86a6b657cfb85abd4a691d1
Rob Harrison <48765695+robjharrison@users.noreply.github.com>
2025-11-10 14:41:01 +0000

    added chmod to align with other scrapes

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@ sudo apt-get update
 # Install Python dependencies
 pip install -r requirements.txt
 
-
+# Ensure run permissions on scrape script
+chmod +x ofsted_send_scrape.py
                                      
 # Install the Python extension for Visual Studio Code
 code --install-extension ms-python.python --force
